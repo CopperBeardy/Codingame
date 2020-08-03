ea1b146f91ac2ad04c2cc9f671ddac3c7f016b3f
davin <copperbeard@protonamail.com>
2020-08-03 20:17:08 +0100

    Bingo solved

---
+++ CodingGame/Solutions/Bingo.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<List<int>> cards = new List<List<int>>();
+        for (int i = 0; i < n; i++)
+        {
+            List<int> c = new List<int>();
+            for (int j = 0; j < 5; j++)
+            {
+                c.AddRange(ParseString(Console.ReadLine()));
+            }
+            cards.Add(c);
+        }
+
+        List<int> calledNumbers = ParseString(Console.ReadLine());
+
+        int quickestLine = 0;
+        int quickestFullhouse = 0;
+
+        foreach (var card in cards)
+        {
+            (int line, int house) result = MarkCards(card, calledNumbers);
+            switch (quickestLine)
+            {
+                case 0:
+                    quickestLine = result.line;
+                    break;
+                default:
+                    if (result.line < quickestLine)
+                    {
+                        quickestLine = result.line;
+                    }
+
+                    break;
+            }
+            switch (quickestFullhouse)
+            {
+                case 0:
+                    quickestFullhouse = result.house;
+                    break;
+                default:
+                    if (result.house < quickestFullhouse)
+                    {
+
+                        quickestFullhouse = result.house;
+                    }
+
+                    break;
+            }
+        }
+
+        Console.WriteLine(quickestLine);
+        Console.WriteLine(quickestFullhouse);
+    }
+
+    private static (int line, int house) MarkCards(List<int> card, List<int> calledNumbers)
+    {
+        int CounterForLine = 0;
+        int CounterForFullHouse = 0;
+        Win forWin = Win.None;
+        (int line, int house) totals = (0, 0);
+        for (int i = 0; i < calledNumbers.Count; i++)
+        {
+            if (forWin == Win.None)
+            {
+                CounterForLine++;
+            }
+
+            for (int j = 0; j < 5; j++)
+            {
+
+                if (card.Contains(calledNumbers[i]))
+                {
+                    card[card.IndexOf(calledNumbers[i])] = 0;
+                }
+                forWin = CheckResult(card, forWin);
+            }
+            CounterForFullHouse++;
+            if (forWin == Win.House)
+            {
+                break;
+            }
+        }
+
+        totals.line = CounterForLine;
+        totals.house = CounterForFullHouse;
+        return totals;
+    }
+
+    private static Win CheckResult(List<int> card, Win win)
+    {
+        if (win != Win.Line)
+        {
+            if (CheckLine(card, 0, 1) || CheckLine(card, 0, 5) || CheckLine(card, 0, 6) || CheckLine(card, 4, 6))
+            {
+                win = Win.Line;
+            }
+        }
+        else if (win == Win.Line)
+        {
+
+            if (CheckHouse(card))
+            {
+                win = Win.House;
+            }
+        }
+        return win;
+    }
+
+    private static bool CheckLine(List<int> card, int start, int increment)
+    {
+        var counter = 0;
+        for (int i = start; i < 5; i++)
+        {
+            for (int j = i; j < card.Count; j += increment)
+            {
+                if (card[j] == 0)
+                {
+                    counter++;
+                    continue;
+                }
+                counter = 0;
+                break;
+            }
+            if (counter == 5)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static bool CheckHouse(List<int> card) =>
+         card.ToArray().Sum() == 0;
+
+    private static List<int> ParseString(string row)
+    {
+        string[] line = row.Split(' ');
+        List<int> temp = new List<int>();
+        temp.AddRange(line.Select(lineItem => int.Parse(lineItem)));
+        return temp;
+    }
+}
+public enum Win
+{
+    None,
+    Line,
+    House
+}
---
+++ CodingGame/Solutions/Copperbeardysolves/RockPaperScissorLizardSpock.cs
@@ -0,0 +1,108 @@
+﻿//using System.Text;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+
+///**
+// * Auto-generated code below aims at helping you parse
+// * the standard input according to the problem statement.
+// **/
+//class Solution
+//{
+
+//    static void Main(string[] args)
+//    {
+
+//        int N = int.Parse(Console.ReadLine());
+//        Player[] players = new Player[N];
+//        for (int i = 0; i < N; i++)
+//        {
+//            string[] inputs = Console.ReadLine().Split(' ');
+//            players[i] = new Player
+//            {
+//                PlayerNumber = int.Parse(inputs[0]),
+//                Move = inputs[1],
+//                Opponents = new List<int>()
+
+//            };
+//        }
+
+
+//        players = Play(players);
+//        string ops = "";
+//        foreach (var player in players[0].Opponents)
+//        {
+//            ops += $"{player} ";
+//        }
+
+
+//        Console.WriteLine(players[0].PlayerNumber);
+//        Console.WriteLine(ops.TrimEnd());
+
+//    }
+
+//    private static Player[] Play(Player[] players)
+//    {
+//        bool playOn = true;
+//        Player[] currentPlayers = players;
+//        Player[] roundWinners;
+//        int index = 0;
+//        while (playOn)
+//        {
+//            roundWinners = new Player[currentPlayers.Length / 2];
+//            index = 0;
+//            for (int i = 0; i < currentPlayers.Length - 1; i += 2)
+//            {
+//                Player player1 = currentPlayers[i];
+//                Player player2 = currentPlayers[i + 1];
+
+//                var result = CheckMove(player1, player2);
+//                if (result)
+//                {
+//                    player1.Opponents.Add(player2.PlayerNumber);
+//                    roundWinners[index] = player1;
+//                }
+//                else
+//                {
+//                    player2.Opponents.Add(player1.PlayerNumber);
+//                    roundWinners[index] = player2;
+//                }
+//                index++;
+//            }
+//            if (roundWinners.Length == 1)
+//            {
+//                playOn = false;
+//            }
+//            currentPlayers = roundWinners;
+//        }
+
+//        return currentPlayers;
+//    }
+
+//    public static bool CheckMove(Player p1, Player p2)
+//    {
+//        //Rock(R)
+//        //Paper(P)
+//        //sCissors(C)
+//        //Lizard(L)
+//        //Spock(S)
+//        return p1.Move.Equals(p2.Move)
+//            ? p1.PlayerNumber < p2.PlayerNumber
+//            : (p1.Move switch
+//            {
+//                "C" => (p2.Move == "P" || p2.Move == "L"),
+//                "P" => (p2.Move == "S" || p2.Move == "R"),
+//                "R" => (p2.Move == "C" || p2.Move == "L"),
+//                "L" => (p2.Move == "S" || p2.Move == "P"),
+//                "S" => (p2.Move == "C" || p2.Move == "R"),
+//                _ => false,
+//            });
+//    }
+//}
+
+//public class Player
+//{
+//    public int PlayerNumber { get; set; }
+//    public string Move { get; set; }
+//    public List<int> Opponents { get; set; }
+//}

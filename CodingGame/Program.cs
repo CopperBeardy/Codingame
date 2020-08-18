fabe7da442b24b74e4e7a53bbffcf220f642a293
davin <copperbeard@protonamail.com>
2020-08-18 12:21:29 +0100

    completed puzzle xp challenge

--- CodingGame/Program.cs
+++ CodingGame/Program.cs
@@ -1,1 +1,34 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+        int L = int.Parse(inputs[0]);
+        int S = int.Parse(inputs[1]);
+        int I = int.Parse(inputs[2]);
+        for (int i = 0; i < L; i++)
+        {
+            string r = Console.ReadLine();
+        }
+        for (int i = 0; i < I; i++)
+        {
+            int c = int.Parse(Console.ReadLine());
+        }
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("answer");
+    }
+}
---
+++ CodingGame/Solutions/Copperbeardysolves/puzzlexp.cs
@@ -0,0 +1,65 @@
+﻿//using System;
+//using System.Linq;
+//using System.IO;
+//using System.Text;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Xml.Linq;
+
+///**
+//    *BlitzProg adapts himself to the new experience system. 
+//    *To stand a chance against the top end tier Codingamers,
+//    *he now needs to create puzzles.
+//    *
+//    *BlitzProg solves his own puzzles once they get accepted
+//    *to maximize experience gain, for a total of 300xp per puzzles.
+//    *
+ 
+//    *
+//    *You're given the current Level and XP needed for BlitzProg to level up. 
+//    *Your goal is to find out what these stats will be after N of his puzzles are approved.
+// **/
+//class Solution
+//{
+//    static void Main(string[] args)
+//    {
+//        int level = int.Parse(Console.ReadLine());
+//        int xp = int.Parse(Console.ReadLine());
+//        int N = int.Parse(Console.ReadLine());
+
+//        N = N * 300;
+
+//        while (N >= xp)
+//        {
+//            N -= xp;
+//            level++;
+//            xp = CalculateXp(level);
+//        }
+//        xp -= N;
+//        /**
+//         * Ashen_Undead02496
+         
+//        *   N = N * 300; 
+//        *   while (N >= Xp) { 
+//        *   N -= Xp;
+//        *   Level++; 
+//        *   Xp = 10 * Level * sqrt(Level); }
+//        *   Xp -= N;
+//        **/
+//        // Write an answer using Console.WriteLine()
+//        // To debug: Console.Error.WriteLine("Debug messages...");
+
+//        Console.WriteLine(level);
+//        Console.WriteLine(Math.Abs(xp));
+//    }
+
+
+
+//    /**
+//     * The xp needed to reach the next level is
+//     *( CurrentLevel * Sqrt(CurrentLevel) * 10 ), rounded down.
+//     **/
+//    public static int CalculateXp(int currentLevel) =>
+//            (int)Math.Floor(currentLevel * Math.Sqrt(currentLevel) * 10);
+
+//}
